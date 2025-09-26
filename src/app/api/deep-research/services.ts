87e2eb6eb5a0f16eed9493aecae3437e3f144fa8
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-09-26 15:21:48 +0530

    Added the exa search api key and its main code in the services file for using it now add the fucnitonliy in research function

--- src/app/api/deep-research/services.ts
+++ src/app/api/deep-research/services.ts
@@ -1,4 +1,8 @@
-import { createOpenRouter } from "@openrouter/ai-sdk-provider";
+import { createOpenRouter } from "@openrouter/ai-sdk-provider"
+import Exa from "exa-js"
+
+
+export const exa = new Exa(process.env.EXA_SEARCH_API_KEY || "");
 
 export const openRouter = createOpenRouter({
   apiKey: process.env.OPENROUTER_API_KEY || "",
