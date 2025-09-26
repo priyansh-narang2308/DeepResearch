f7b8815f3854832908a61de77d548bd117be01a8
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-09-26 14:59:59 +0530

    Added the research State and the call modal function and used the prompt to do so and added all the prompts from ai to do so

--- src/app/api/deep-research/model-caller.ts
+++ src/app/api/deep-research/model-caller.ts
@@ -1,8 +1,25 @@
+import { generateObject } from "ai";
 import { ModalCallOptions, ResearchState } from "./types";
+import { openRouter } from "./services";
 
 export async function callModel<T>(
   { model, prompt, system, schema }: ModalCallOptions<T>,
   researchState: ResearchState
 ): Promise<T | string> {
-  return "";
+  // Parse the questions using generate object from ai-sdk
+  if (!schema) {
+    throw new Error("Schema must be provided to callModel.");
+  }
+  const { object, usage } = await generateObject({
+    model: openRouter(model),
+    prompt,
+    system,
+    schema: schema,
+  });
+
+  //   Check for the total tokens needed! Usage taken from the ai-sdk
+  researchState.tokenUsed += usage.totalTokens!;
+  researchState.completedSteps++;
+
+  return object;
 }
--- src/app/api/deep-research/research-functions.ts
+++ src/app/api/deep-research/research-functions.ts
@@ -2,15 +2,26 @@ import { generateObject } from "ai";
 import { ResearchState } from "./types";
 import { openRouter } from "./services";
 import z from "zod";
+import { callModel } from "./model-caller";
+import { getPlanningPrompt, PLANNING_SYSTEM_PROMPT } from "./prompts";
 
 export async function generateSearchQueries(researchState: ResearchState) {
-  // Parse the questions using generate object from ai-sdk
-  const { object } = await generateObject({
-    model: openRouter("x-ai/grok-4-fast:free"),
-    prompt: "",
-    system: "",
-    schema: z.object({
-      questions: z.array(z.string()),
-    }),
-  });
+  const results = await callModel(
+    {
+      model: "mistralai/mistral-small-3.2-24b-instruct:free",
+      prompt: getPlanningPrompt(
+        researchState.topic,
+        researchState.clarificationsText
+      ),
+      system: PLANNING_SYSTEM_PROMPT,
+      schema: z.object({
+        searchQueries: z.array(z.string()).describe(
+          "The search queries that can be used to find the most relevant content which can be used to write the most comprehensive report on the given topic. (max 3 queries)" //how many queries will pass
+        ),
+      }),
+    },
+    researchState
+  );
+
+  return results;
 }
